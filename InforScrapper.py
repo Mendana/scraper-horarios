d985c410a126ef01436f01538ee0904ec169e97f
mendana <diegodiazmenda@gmail.com>
2025-04-13 12:48:22 +0200

    A

--- InforScrapper.py
+++ InforScrapper.py
@@ -7,7 +7,7 @@ def run():
     os.makedirs("dataI", exist_ok=True)
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(accept_downloads=True)
         page = context.new_page()
 
